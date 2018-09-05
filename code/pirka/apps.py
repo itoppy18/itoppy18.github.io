57c6e3c7b592afbbc973b56253dc9b834ad458b2
Mamoru Itoi <itoppy18@gmail.com>
2018-09-05 18:55:03 +0900

    pirka update

--- code/pirka/apps.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bon/python3
-#config:utf-8
-#kanto kor pirka nociw AI system α0.0.0.0a source code
-#apps.py:各種機能
-#ハングリーであれ。愚かであれ。-スティーブ・ジョブズ
-#created by @itoppy18
--- code/pirka/response.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-#config:utf-8
-#kanto kor pirka nociw AI system α0.0.0.0a source code
-#response.py:会話の応答の生成
-#稼ぐために学んではいけない。学ぶために学べ。-ビル・ゲイツ
-#created by @itoppy18
-
