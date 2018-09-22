56da005ecb4f2e8b10f5c3d664f31e49e72395d5
Mamoru Itoi <itoppy18@gmail.com>
2018-09-22 15:12:20 +0900

    wii update

--- code/wii.py
+++ code/wii.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get(https://itoppy18.github.io/code.html)
+html = requests.get("https://itoppy18.github.io/code/wii.html")
 soup = BeautifulSoup(html.text, "html.parser")
-print(html.body.p.string)
+print(soup.p.string)
 
